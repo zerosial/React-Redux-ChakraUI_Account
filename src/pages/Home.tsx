49373e2cb024112470c3cb7ea3e14e0e81065469
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-08-16 16:49:13 +0900

    feat: map 함수에 key 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,15 @@ const Home = () => {
     <Container maxW={"100%"} p={"16px"}>
       <Heading mb={4}>Home</Heading>
       {account.map((user: AccountState, index: number) => (
-        <Box bg="blue.300" w="50%" p={4} color="white" mb={4} borderRadius={4}>
+        <Box
+          key={index}
+          bg="blue.300"
+          w="50%"
+          p={4}
+          color="white"
+          mb={4}
+          borderRadius={4}
+        >
           <Text>회원번호 {index + 1}번</Text>
           <Text>{user.id}</Text>
           <Text>{user.password}</Text>
